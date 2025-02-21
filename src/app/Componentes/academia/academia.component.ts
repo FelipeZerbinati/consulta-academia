eb9020c4c7f504807af642c7dfff1bf4519205c1
FelipeZerbinati <fezerba@gmail.com>
2025-02-21 20:56:13 -0300

    componente academia criado

---
+++ src/app/Componentes/academia/academia.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-academia',
+  imports: [],
+  templateUrl: './academia.component.html',
+  styleUrl: './academia.component.css'
+})
+export class AcademiaComponent {
+
+}
